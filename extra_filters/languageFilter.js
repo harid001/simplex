0e8db8ecfa7215ac46a3299f6db04d6eb5ab675b
Hari Dhanushkodi <hari97.nd@gmail.com>
2014-07-20 11:25:46 -0700

    fix microsoft api

--- extra_filters/languageFilter.js
+++ extra_filters/languageFilter.js
@@ -6,7 +6,7 @@ function changeLang(word,language){
 
 	$.ajax({
 		// url : 'https://api.datamarket.azure.com/Bing/MicrosoftTranslator/v1/Translate?Text=%27' + word + '%27&To=%27' + lang_map[language] + '%27',
-		url : 'https://api.datamarket.azure.com/Bing/MicrosoftTranslator/v1/Translate?Text=%27' + word + '%27&To=%27' + lang_map[language] + '%27',
+		url : 'https://user:6ZZrSX4PmMGHVqbuIoZFzmVYViTTixYHDT4hEx19tuk@api.datamarket.azure.com/Bing/MicrosoftTranslator/v1/Translate?Text=%27' + word + '%27&To=%27' + lang_map[language] + '%27',
 		// url : 'https://api.datamarket.azure.com/Bing/MicrosoftTranslator/v1/Translate?Text=%27hello%27&To=%27es%27',
 		success : function(result) { data = result; },
 		async : false
